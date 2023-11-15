72d9191b62c9ef685fcb33c1b17b6ae7f8ff9393
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2023-11-14 20:32:13 -0800

    Update title in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     new ESLintPlugin(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: "Shape Tracker",
+      title: "Giphy API",
       template: "./src/index.html",
       inject: "body",
     }),
